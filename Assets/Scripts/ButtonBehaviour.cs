aaf7abd62c41096afde08ae4e5685e874bf7f6f0
Arpit Mathur <arpitmathur19@gmail.com>
2023-07-09 12:56:04 -0400

    second level

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -5,92 +5,119 @@ using UnityEngine;
 public class ButtonBehaviour : MonoBehaviour
 {
 
-    private bool player_near_switch;
+  private bool player_near_switch;
 
-    private GameObject environment;
+  private GameObject environment;
 
-    private GameObject[] triggered_platforms;
+  private GameObject[] triggered_platforms;
 
-    [SerializeField] private bool conditional_switch;
+  [SerializeField] private bool conditional_switch;
 
-    [SerializeField] private EnvState active_state;
+  [SerializeField] private EnvState active_state;
 
 
-    private bool isPressed = false;
-    public Sprite PressedSprite;
-    public Sprite NotPressedSprite;
+  private bool isPressed = false;
+  public Sprite PressedSprite;
+  public Sprite NotPressedSprite;
 
-    private BoxCollider2D switch_bc;
-    private SpriteRenderer switch_sr;
+  private BoxCollider2D switch_bc;
+  private SpriteRenderer switch_sr;
 
-    private Animator anim;
+  private Animator anim;
 
-    private AudioSource click_sound;
+  private AudioSource click_sound;
 
-    void Start() {
-        player_near_switch = false;
-        environment = GameObject.FindWithTag("Environment");
-        triggered_platforms =  GameObject.FindGameObjectsWithTag("TriggeredPlatform");
-        switch_bc = GetComponent<BoxCollider2D>();
-        switch_sr = GetComponent<SpriteRenderer>();
-        anim = GetComponent<Animator>();
-        click_sound = GetComponent<AudioSource>();
+  [SerializeField] private bool contControl;
+
+  void Start()
+  {
+    player_near_switch = false;
+    environment = GameObject.FindWithTag("Environment");
+    triggered_platforms = GameObject.FindGameObjectsWithTag("TriggeredPlatform");
+    switch_bc = GetComponent<BoxCollider2D>();
+    switch_sr = GetComponent<SpriteRenderer>();
+    anim = GetComponent<Animator>();
+    click_sound = GetComponent<AudioSource>();
+  }
+
+  void Update()
+  {
+    if (player_near_switch)
+    {
+      isPressed = true;
+    }
+    else
+    {
+      isPressed = false;
     }
 
-    void Update() {
-        if (player_near_switch) {
-            isPressed= true;
-        } else {
-            isPressed = false;
-        }
-
-        if (conditional_switch) {
-            ShowSwitchOnCondition();
-        }
-
-        if (isPressed) {
-            switch_sr.sprite = PressedSprite;
-        } else {
-            switch_sr.sprite = NotPressedSprite;
-        }
+    if (conditional_switch)
+    {
+      ShowSwitchOnCondition();
     }
 
-    private void ShowSwitchOnCondition() {
-        EnvState currentState = environment.GetComponent<EnvironmentState>().GetState();
-        if (active_state == currentState) {
-            switch_bc.enabled = true;
-            switch_sr.enabled = true;
-        } else {
-            switch_bc.enabled = false;
-            switch_sr.enabled = false;
-        }
+    if (isPressed)
+    {
+      switch_sr.sprite = PressedSprite;
+      for (int i = 0; i < triggered_platforms.Length; i++)
+        triggered_platforms[i].GetComponent<PlatformMovement>().contControl = contControl;
     }
+    else
+    {
+      switch_sr.sprite = NotPressedSprite;
+      for (int i = 0; i < triggered_platforms.Length; i++)
+        triggered_platforms[i].GetComponent<PlatformMovement>().contControl = false;
+    }
+  }
 
-    private void FlipSwitch() {
-        click_sound.Play();
+  private void ShowSwitchOnCondition()
+  {
+    EnvState currentState = environment.GetComponent<EnvironmentState>().GetState();
+    if (active_state == currentState)
+    {
+      switch_bc.enabled = true;
+      switch_sr.enabled = true;
     }
+    else
+    {
+      switch_bc.enabled = false;
+      switch_sr.enabled = false;
+    }
+  }
 
-    private void OnTriggerEnter2D(Collider2D collider) {
-        if (collider.CompareTag("Player")) {
-            player_near_switch = true;
-            FlipSwitch();
-            MoveAllTriggeredPlatforms();
-        }
-    }
+  private void FlipSwitch()
+  {
+    click_sound.Play();
+  }
 
-    private void OnTriggerExit2D(Collider2D collider) {
-        if (collider.CompareTag("Player")) {
-            player_near_switch = false;
-            FlipSwitch();
-            MoveAllTriggeredPlatforms();
-        }
-    }
+  private void OnTriggerEnter2D(Collider2D collider)
+  {
+    // if (collider.CompareTag("Player")) {
+    player_near_switch = true;
+    FlipSwitch();
+    if (!contControl)
+      MoveAllTriggeredPlatforms();
+    // }
+  }
 
-    private void MoveAllTriggeredPlatforms() {
-        if (triggered_platforms.Length > 0) {
-            for (int i = 0; i < triggered_platforms.Length; i++) {
-                triggered_platforms[i].GetComponent<PlatformTriggeredMovement>().PlatformMoveToNext();
-            }
-        }
+  private void OnTriggerExit2D(Collider2D collider)
+  {
+    // if (collider.CompareTag("Player")) {
+    player_near_switch = false;
+    FlipSwitch();
+    if (!contControl)
+      MoveAllTriggeredPlatforms();
+    // }
+  }
+
+  private void MoveAllTriggeredPlatforms()
+  {
+    if (triggered_platforms.Length > 0)
+    {
+      for (int i = 0; i < triggered_platforms.Length; i++)
+      {
+        triggered_platforms[i].GetComponent<PlatformTriggeredMovement>().PlatformMoveToNext();
+      }
     }
+  }
 }
--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-    [SerializeField] private GameObject[] waypoints;
-    private GameObject current_wapoint;
-    private int current_waypoint_index;
-    [SerializeField] private float y_speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        current_waypoint_index = 0;
-        current_wapoint = waypoints[current_waypoint_index];
-    }
+  [SerializeField] private GameObject[] waypoints;
+  private GameObject current_wapoint;
+  private int current_waypoint_index;
+  [SerializeField] private float y_speed;
 
-    // Update is called once per frame
-    void Update()
+  public bool contControl = false;
+  // Start is called before the first frame update
+  void Start()
+  {
+    current_waypoint_index = 0;
+    current_wapoint = waypoints[current_waypoint_index];
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (contControl)
     {
-        if (Vector2.Distance(transform.position, current_wapoint.transform.position) > 0.1f) {
-            transform.position = Vector2.MoveTowards(transform.position, current_wapoint.transform.position, y_speed * Time.deltaTime);
-        } else {
-            current_waypoint_index = (current_waypoint_index + 1) % waypoints.Length;
-            current_wapoint = waypoints[current_waypoint_index];
-        }
+      if (Vector2.Distance(transform.position, current_wapoint.transform.position) > 0.1f)
+      {
+        transform.position = Vector2.MoveTowards(transform.position, current_wapoint.transform.position, y_speed * Time.deltaTime);
+      }
+      else
+      {
+        current_waypoint_index = (current_waypoint_index + 1) % waypoints.Length;
+        current_wapoint = waypoints[current_waypoint_index];
+      }
     }
+  }
 }
